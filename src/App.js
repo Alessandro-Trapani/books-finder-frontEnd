19cbb19fbb083f643b84c000ade60a1c64f89abe
Alessandro <alessandro.trapani03@gmail.com>
2025-07-10 09:32:59 +0200

    fix: change Router import from BrowserRouter to HashRouter in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import BookSection from "./components/BookSection";
 import Book from "./pages/Book";
